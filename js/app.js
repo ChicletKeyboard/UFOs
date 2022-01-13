c746210a51af4a6faab44be76166908dbbcfe477
Jason Klein <93780784+ChicletKeyboard@users.noreply.github.com>
2022-01-12 19:04:17 -0600

    JS code for HTML website

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML using d3
+//      d3.select tells JS to look for <tbody> </tbody> tags in the HTML
+var tbody = d3.select("tbpdy");
+
+function buldTable(data) {
+
+    
+}
